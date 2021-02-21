d7ebadd511aab694aab7db5fc3fd09014cf236bd
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2021-02-21 14:41:42 -0300

    Alguns testes básicos realizados com o Eloquent

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/model', function() {
+    // $products = \App\Models\Product::all(); // select * from products
+
+    // $user = new \App\Models\User();
+    // $user->name = '';
+    // $user->email = '';
+    // $user->password = '';
+    // $user->save();
+
+    // return \App\Models\User::where('name', 'Jennyfer Klein PhD')->first();
+
+    // return \App\Models\User::paginate(5);
+
+    // Mass Assignment
+    // \App\Models\User::create([
+    //     'name' => 'Abacate',
+    //     'email' => 'abacate@email.com',
+    //     'password' => bcrypt('naotempassword')
+    // ]);
+
+    // O Mass Update funciona analogamente
+});
