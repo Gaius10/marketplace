3734ae4bccec31365413c8e521fb3fe94bebb51e
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2021-03-06 18:08:08 -0300

    Primeiras implementações de estilização usando Tailwind CS

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -1,36 +1,71 @@
-<form action="/admin/stores/store" method="post">
-    <h1>Criar Loja</h1>
-    @csrf
-    <div>
-        <label>Nome Loja</label>
-        <input type="text" name="name">
-    </div>
-    <div>
-        <label>Descrição</label>
-        <input type="text" name="description">
-    </div>
-    <div>
-        <label>Telefone</label>
-        <input type="text" name="phone">
-    </div>
-    <div>
-        <label>Celular/Whatsapp</label>
-        <input type="text" name="mobile_phone">
-    </div>
-    <div>
-        <label>Slug</label>
-        <input type="text" name="slug">
-    </div>
-    <div>
-        <label>Usuário</label>
-        <select name="user">
-            @foreach($users as $user)
-            <option value="{{$user->id}}">{{$user->name}}</option>
-            @endforeach
-        </select>
-    </div>
+@extends('layouts.app')
 
-    <div>
-        <button type="submit">Criar Loja <?= -12 < null ?></button>
+@section('content')
+    <div class="md:mt-20 sm:mt-0 flex flex-col items-center">
+        <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="md:col-span-1">
+                <div class="px-4 sm:px-0">
+                    <h3 class="text-lg font-medium leading-6 text-gray-900">Criar Loja</h3>
+                    <p class="mt-1 text-sm text-gray-600">
+                        Formulário para criação de loja
+                    </p>
+                </div>
+            </div>
+            <div class="mt-5 md:mt-0 md:col-span-3">
+                <form action="/admin/stores/store" method="POST">
+                    @csrf
+                    <div class="shadow overflow-hidden sm:rounded-md">
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            <div class="grid grid-cols-6 gap-6">
+                                <div class="col-span-12 md:col-span-6">
+                                    <label for="name" class="block text-sm font-medium text-gray-700">Nome da loja</label>
+                                    <input type="text" name="name" id="name" autocomplete="given-name"
+                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+
+                                <div class="col-span-12 md:col-span-6">
+                                    <label for="description" class="block text-sm font-medium text-gray-700">Descrição da loja</label>
+                                    <textarea id="description" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                                </div>
+
+                                <div class="col-span-12 md:col-span-3">
+                                    <label for="phone" class="block text-sm font-medium text-gray-700">Telefone</label>
+                                    <input id="phone" name="phone" autocomplete="phone" type="text"
+                                        class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                </div>
+
+                                <div class="col-span-12 md:col-span-3">
+                                    <label for="mobile_phone" class="block text-sm font-medium text-gray-700">Celular</label>
+                                    <input id="mobile_phone" name="mobile_phone" autocomplete="mobile_phone" type="text"
+                                        class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                </div>
+
+                                <div class="col-span-12 md:col-span-3">
+                                    <label for="slug" class="block text-sm font-medium text-gray-700">Slug</label>
+                                    <input id="slug" name="slug" autocomplete="slug" type="text"
+                                        class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                </div>
+
+                                <div class="col-span-12 md:col-span-3">
+                                    <label for="user" class="block text-sm font-medium text-gray-700">Usuário</label>
+                                    <select id="user" name="user" autocomplete="user" type="text"
+                                        class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                        @foreach ($users as $user)
+                                            <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                            <button type="submit"
+                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-</form>
+@endsection
